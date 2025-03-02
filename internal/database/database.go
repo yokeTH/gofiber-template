fd05f174467f85a204e621df129e18660063db12
Thanapon Johdee <sern.dev@gmail.com>
2025-03-02 21:48:03 +0700

    feat: add pagination functionality to database

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,9 +1,30 @@
 package database
 
 import (
+	"errors"
+	"math"
+
 	"gorm.io/gorm"
 )
 
 type Database struct {
 	*gorm.DB
+}
+
+func (db *Database) Paginate(value any, limit, page int, order string) (func(db *Database) *Database, int, int, error) {
+	var totalRows int64
+	if err := db.Model(value).Count(&totalRows).Error; err != nil {
+		return nil, 0, 0, err
+	}
+
+	totalPages := int(math.Ceil(float64(totalRows) / float64(limit)))
+	offset := (page - 1) * limit
+
+	if page > totalPages {
+		offset = (totalPages - 1) * limit
+		return func(db *Database) *Database { return &Database{db.Offset(offset).Limit(limit).Order(order)} }, totalPages, int(totalRows), errors.New("page exceeded")
+	}
+
+	return func(db *Database) *Database { return &Database{db.Offset(offset).Limit(limit).Order(order)} }, totalPages, int(totalRows), nil
+
 }
