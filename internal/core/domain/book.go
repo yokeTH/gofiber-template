69936b7497ce72fa0a6cb65c8dbdb9103afc80ce
Thanapon Johdee <sern.dev@gmail.com>
2025-03-07 21:46:37 +0700

    refactor: simplify Book model and add DTO for book requests and responses

--- internal/core/domain/book.go
+++ internal/core/domain/book.go
@@ -1,16 +1,11 @@
 package domain
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Book struct {
-	ID        uint           `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time      `json:"-"`
-	UpdatedAt time.Time      `json:"-"`
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
-	Title     string         `json:"title,omitempty"`
-	Author    string         `json:"author,omitempty"`
+	gorm.Model
+	Title  string
+	Author string
 }
