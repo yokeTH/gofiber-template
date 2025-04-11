b826d6d0b6eaebce0bf379118c3e1eb2d968e672
Thanapon Johdee <sern.dev@gmail.com>
2025-04-12 01:48:33 +0700

    Refactor S3 storage implementation
    Added support for multiple storage buckets and simplified the storage package
    by consolidating R2-specific code into a generic S3-compatible implementation.
    Environment variables were standardized and unused fields removed.

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,14 +9,15 @@ import (
 	"github.com/yokeTH/gofiber-template/pkg/storage"
 )
 
-type Config struct {
-	Server server.Config    `envPrefix:"SERVER_"`
-	PSQL   db.DBConfig      `envPrefix:"POSTGRES_"`
-	R2     storage.R2Config `envPrefix:"R2_"`
+type config struct {
+	Server        server.Config  `envPrefix:"SERVER_" env:"required"`
+	PSQL          db.DBConfig    `envPrefix:"POSTGRES_" env:"required"`
+	PublicBucket  storage.Config `envPrefix:"PUBLIC_" env:"required"`
+	PrivateBucket storage.Config `envPrefix:"PRIVATE_" env:"required"`
 }
 
-func Load() *Config {
-	config := &Config{}
+func Load() *config {
+	config := &config{}
 
 	if err := godotenv.Load(); err != nil {
 		log.Warnf("Unable to load .env file: %s", err)
