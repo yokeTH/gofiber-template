05497c3b43dd9c306c9879bca979d32f3deeeff9
Thanapon Johdee <sern.dev@gmail.com>
2025-04-12 04:16:12 +0700

    Update file_handler.go

--- internal/adaptor/handler/file_handler.go
+++ internal/adaptor/handler/file_handler.go
@@ -20,6 +20,18 @@ func NewFileHandler(uc file.FileUseCase, private storage.Storage, public storage
 	}
 }
 
+// CreateFile godoc
+//
+//	@summary		CreateFile
+//	@description	create private file by upload file multipart-form field name file
+//	@tags			file
+//	@accept			x-www-form-urlencoded
+//	@produce 		json
+//	@param			file	formData 	file	true "file data"
+//	@success 		201	{object}	dto.SuccessResponse[dto.FileResponse]	"Created"
+//	@failure		400	{object}	dto.ErrorResponse	"Bad Request"
+//	@failure 		500	{object}	dto.ErrorResponse	"Internal Server Error"
+//	@Router /files/private [post]
 func (h *fileHandler) CreatePrivateFile(c *fiber.Ctx) error {
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -36,6 +48,18 @@ func (h *fileHandler) CreatePrivateFile(c *fiber.Ctx) error {
 	return c.Status(201).JSON(dto.Success(response))
 }
 
+// CreatePublicFile godoc
+//
+//	@summary		CreatePublicFile
+//	@description	create public file by upload file multipart-form field name file
+//	@tags			file
+//	@accept			x-www-form-urlencoded
+//	@produce 		json
+//	@param			file	formData 	file	true "file data"
+//	@success 		201	{object}	dto.SuccessResponse[dto.FileResponse]	"Created"
+//	@failure		400	{object}	dto.ErrorResponse	"Bad Request"
+//	@failure 		500	{object}	dto.ErrorResponse	"Internal Server Error"
+//	@Router /files/private [post]
 func (h *fileHandler) CreatePublicFile(c *fiber.Ctx) error {
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -52,6 +76,18 @@ func (h *fileHandler) CreatePublicFile(c *fiber.Ctx) error {
 	return c.Status(201).JSON(dto.Success(response))
 }
 
+// GetFiles godoc
+//
+//	@summary		GetBooks
+//	@description	get files information
+//	@tags			file
+//	@produce		json
+//	@Param			limit	query	int	false	"Number of history to be retrieved"
+//	@Param			page	query	int	false	"Page to retrieved"
+//	@response		200	{object}	dto.PaginationResponse[dto.FileResponse]	"OK"
+//	@response		400	{object}	dto.ErrorResponse	"Bad Request"
+//	@response		500	{object}	dto.ErrorResponse	"Internal Server Error"
+//	@Router /files [get]
 func (h *fileHandler) List(c *fiber.Ctx) error {
 	page, limit := extractPaginationControl(c)
 	files, last, total, err := h.fileUseCase.List(limit, page)
@@ -64,6 +100,17 @@ func (h *fileHandler) List(c *fiber.Ctx) error {
 	return c.Status(200).JSON(dto.SuccessPagination(response, page, last, limit, total))
 }
 
+// GetFile godoc
+//
+//	@summary		Get file url
+//	@description	get file information and url
+//	@tags			file
+//	@produce		json
+//	@Param			id	path	int	true	"file id"
+//	@success 		200	{object}	dto.SuccessResponse[dto.FileResponse]	"OK"
+//	@failure		400	{object}	dto.ErrorResponse	"Bad Request"
+//	@failure 		500	{object}	dto.ErrorResponse	"Internal Server Error"
+//	@Router /books/{id} [get]
 func (h *fileHandler) GetInfo(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
